91b0454bbc3af13903d4332f5f65475edc80e70a
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-12-11 16:20:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,8 @@ fetch = require('node-fetch');
 const express = require("express");
 
 const app = express();
-const DB_USER = process.env.DB_USER || "missing-parameter";
-const DB_NAME = process.env.DB_NAME || "missing-parameter";
-const DB_PASS = process.env.DB_PASS || "missing-parameter";
-const HOST = process.env.HOST || "missing-parameter"
 
 
-const { Client } = require('pg')
-
-const client = new Client ({
-  user: DB_USER,
-  password: DB_PASS,
-  host: HOST,
-  port: 5432,
-  database: DB_NAME
-})
-
-client.connect()
 
 
 
@@ -28,17 +13,10 @@ app.get("/", (req, res) => {
   res.send(
     "<h1>GlueOps June 21, 2022</h1>" +
       "<h2> DB </h2>" +
-	"DB: <b>" + DB_NAME + "</b><br />"
+	"DB: <b>" + "DB missing" + "</b><br />"
   );
 });
 
-app.get('/database', (request, response) => {
-    client.query('SELECT * FROM pg_catalog.pg_tables', (error, result) => {
-        if (error) throw error;
- 
-        response.send(result.rows[0]);
-    });
-});
 
 app.get('/ip', (request, response) => {
     fetch('https://api.ipify.org/?format=json')
